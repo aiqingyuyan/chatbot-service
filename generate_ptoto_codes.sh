c3794c5e069dc286e313eb25dc5a196ba6fb62c0
Yan <yuy6@miamioh.edu>
2019-09-16 00:00:09 -0400

    style change

--- generate_ptoto_codes.sh
+++ generate_ptoto_codes.sh
@@ -1,8 +1,8 @@
 if [ -d "./app/generated" ]
 then
-		rm -rf ./app/generated/*
+	rm -rf ./app/generated/*
 else
-		mkdir -p ./app/generated
+	mkdir -p ./app/generated
 fi
 
 python3 -m grpc_tools.protoc -I./protos --python_out=./app/generated --grpc_python_out=./app/generated ./protos/chatbot.proto
